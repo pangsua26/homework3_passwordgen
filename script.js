7a484441e442e75fe73d9e9a84bedf9a7caae117
Pangsua Vang <pangsua26@yahoo.com>
2020-04-17 17:16:14 -0500

    added html css and variables

---
+++ script.js
@@ -0,0 +1,23 @@
+function generate(){
+
+    var Char = prompt("How many characters would you like your password to contain?");
+    var special = confirm("Click OK to confirm including special charaters.");
+    var numeric = confirm("Click OK to confirm including numeric characters.");
+    var lowercase= confirm("Click OK to confirm incluidng lowercase characters.");
+    var upper= confirm("Click OK to confirm including uppercase characters.")
+    
+
+    var length = "20";
+    var character= "!@#$%^&*()_+~`|}{[]\:;?><,./-=";
+    var numbers = "0123456789";
+    var stringLow= "abcdefghijklmnopqrstuvwxyz";
+    var stringUp= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    
+
+
+
+
+
+
+
+}
