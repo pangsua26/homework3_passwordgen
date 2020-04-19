d5fc14f710c35b319337c365c3439c73092d25f7
Pangsua Vang <pangsua26@yahoo.com>
2020-04-18 23:19:38 -0500

    updated all files

--- script.js
+++ script.js
@@ -1,23 +1,60 @@
-function generate(){
+var charArray=[];
+var specialChar=["!","@","#","$","%","^","&","*","(",")","{","}","|","[","]",";","'",":","<",">","?","/"];
+var numericChar=["1","2","3","4","5","6","7","8","9","0"];
+var lowerCaseChar=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upperCaseChar=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","W","X","Y","Z"];
 
-    var Char = prompt("How many characters would you like your password to contain?");
-    var special = confirm("Click OK to confirm including special charaters.");
-    var numeric = confirm("Click OK to confirm including numeric characters.");
-    var lowercase= confirm("Click OK to confirm incluidng lowercase characters.");
-    var upper= confirm("Click OK to confirm including uppercase characters.")
+
+function generatePassword() {
+
+    var passwordString="";
+
+    var passwordLength=parseInt(prompt("How many characters would you like your password to contain?"));
+    while(passwordLength < 8 || passwordLength > 128 || typeof(passwordLength) != "number" || passwordLength === NaN || passwordLength === null);
+
+    var special=confirm("Click OK to confirm including special charaters.");
+    var numeric=confirm("Click OK to confirm including numeric characters.");
+    var lowerCase=confirm("Click OK to confirm incluidng lowercase characters.");
+    var upperCase=confirm("Click OK to confirm including uppercase characters.");
     
 
-    var length = "20";
-    var character= "!@#$%^&*()_+~`|}{[]\:;?><,./-=";
-    var numbers = "0123456789";
-    var stringLow= "abcdefghijklmnopqrstuvwxyz";
-    var stringUp= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    if (lowerCase==true){
+        caseArray.push(lowerCaseChar);
     
+    }
+    if (upperCase==true){
+        caseArray.push(upperCaseChar)
+    
+    }
+    if (numeric==true){
+        caseArray.push(numericChar)
+        
+    }
+    if (special==true){
+        caseArray.push(specialChar)
+    }
 
+    for(let i=0;i<passwordLength;i++){
 
+        let randomCharArrayNum;
+        let selectedCharArray;
+        let randomCharNum;
+        let randomChar;
 
+        randomCharArrayNum= parseInt(Math.floor(Math.random()*caseArray.length));
 
+        selectedCharArray=caseArray[randomCharArrayNum];
 
+        randomCharNum=Math.floor(Math.random()*selectedCharArray.length);
 
+        randomChar=selectedCharArray[randomCharNum];
+
+        passwordString+=randomChar;
+    }
+    passwordEntry.textContent=passwordString;  
 
 }
+
+var generateButton=document.getElementById("generateButton");
+var passwordEntry=document.getElementById("passwordEntry");
+generateButton.onclick = generatePassword;
